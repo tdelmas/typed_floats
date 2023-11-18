135a3c65f983576a2275dad5538fab6a72257872
Tom Delmas <1955774+tdelmas@users.noreply.github.com>
2023-11-18 17:05:12 +0000

    assert_relative_eq
    Signed-off-by: Tom Delmas <1955774+tdelmas@users.noreply.github.com>

--- typed_floats_macros/src/gen_tests.rs
+++ typed_floats_macros/src/gen_tests.rs
@@ -157,7 +157,7 @@ pub fn generate_tests_self(float_type: &'static str, filter: &str) -> proc_macro
                 if original.is_nan() {
                     assert_eq!(original.is_nan(), as_float.is_nan());
                 } else {
-                    assert_eq!(as_float, original);
+                    assert_relative_eq!(as_float, original, 1e-6);
                 }
 
                 // Add the result to the list of values to check is the result type is as strict as possible
