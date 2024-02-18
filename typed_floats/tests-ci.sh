ed087bbf28a7feffee4e0671874fa7558ec30a55
Tom <1955774+tdelmas@users.noreply.github.com>
2024-02-18 12:53:14 +0000

    Faster CI for PR

--- typed_floats/tests-ci.sh
+++ typed_floats/tests-ci.sh
@@ -8,19 +8,8 @@ set -x
 
 cd $(dirname $0)
 
-# First check that it builds with all features combinations
-cargo build
-cargo build --no-default-features
-cargo build --no-default-features --features serde
-cargo build --no-default-features --features libm
-cargo build --no-default-features --features serde,libm
-cargo build --no-default-features --features serde,std,libm
-
-
 # Default tests (with std feature only)
 cargo test
-
-# no-std tests
 
 ## Test without any feature (no std)
 cargo test --no-default-features
@@ -29,25 +18,6 @@ cargo test --no-default-features --features libm
 
 # Test serde (with std)
 cargo test --verbose --features serde
-
-cargo build --no-default-features --features serde,std,libm
-
-# Test in release mode
-
-## Only check that it builds with all features combinations
-cargo build --release
-cargo build --release --no-default-features
-cargo build --release --no-default-features --features serde
-cargo build --release --no-default-features --features libm
-cargo build --release --no-default-features --features serde,libm
-cargo build --release --no-default-features --features serde,std,libm
-
-cargo test --no-run --release
-cargo test --no-run --release --no-default-features
-cargo test --no-run --release --no-default-features --features serde
-cargo test --no-run --release --no-default-features --features libm
-cargo test --no-run --release --no-default-features --features serde,libm
-cargo test --no-run --release --no-default-features --features serde,std,libm
 
 ## Run tests with the two main features: serde and std
 cargo test --release --no-default-features --features serde,std
