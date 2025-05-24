5bfabc43938a041f40a4eed195450ebd5a0e38db
Tom <1955774+tdelmas@users.noreply.github.com>
2025-05-24 19:13:15 +0000

    Clean ord implementation
    Signed-off-by: GitHub <noreply@github.com>

--- typed_floats/src/types/impls/ord.rs
+++ typed_floats/src/types/impls/ord.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 
 macro_rules! impl_ord {
-    ($test:ident, $type:ident) => {
+    ($type:ident) => {
         impl Ord for $type<f32> {
             #[inline]
             fn cmp(&self, other: &Self) -> core::cmp::Ordering {
@@ -35,7 +35,12 @@ macro_rules! impl_ord {
                 }
             }
         }
+    };
+}
 
+
+macro_rules! impl_partial_ord {
+    ($type:ident) => {
         impl PartialOrd for $type<f32> {
             #[inline]
             fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
@@ -77,120 +82,31 @@ macro_rules! impl_ord {
                 self.partial_cmp(&other.0)
             }
         }
-
-        #[cfg(test)]
-        mod $test {
-            extern crate std;
-            use crate::*;
-            use std::vec::Vec; // Required for the tests to compile in no_std mode
-
-            fn is_sorted<T: Ord>(slice: &[T]) -> bool {
-                slice.windows(2).all(|w| w[0] <= w[1])
-            }
-
-            #[test]
-            fn f32() {
-                let mut values = tf32::get_test_values()
-                    .iter()
-                    .map(|&x| tf32::$type::new(x))
-                    .filter_map(|x| x.ok())
-                    .collect::<Vec<_>>();
-
-                values.sort();
-
-                assert!(is_sorted(&values));
-
-                let reversed = values.iter().rev().collect::<Vec<_>>();
-
-                assert!(!is_sorted(&reversed));
-
-                let mut sorted = reversed.clone();
-                sorted.sort();
-
-                assert!(is_sorted(&sorted));
-            }
-
-            #[test]
-            fn f64() {
-                let mut values = tf64::get_test_values()
-                    .iter()
-                    .map(|&x| tf64::$type::new(x))
-                    .filter_map(|x| x.ok())
-                    .collect::<Vec<_>>();
-
-                values.sort();
-
-                assert!(is_sorted(&values));
-
-                let reversed = values.iter().rev().collect::<Vec<_>>();
-
-                assert!(!is_sorted(&reversed));
-
-                let mut sorted = reversed.clone();
-                sorted.sort();
-
-                assert!(is_sorted(&sorted));
-            }
-
-            #[test]
-            fn cmp_with_others() {
-                let values_non_nan = tf64::get_test_values()
-                    .iter()
-                    .map(|&x| tf64::NonNaN::new(x))
-                    .filter_map(|x| x.ok())
-                    .collect::<Vec<_>>();
-
-                let values_non_zero_non_nan = tf64::get_test_values()
-                    .iter()
-                    .map(|&x| tf64::NonZeroNonNaN::new(x))
-                    .filter_map(|x| x.ok())
-                    .collect::<Vec<_>>();
-
-                let values = tf64::get_test_values()
-                    .iter()
-                    .map(|&x| tf64::$type::new(x))
-                    .filter_map(|x| x.ok())
-                    .collect::<Vec<_>>();
-
-                for a in &values {
-                    for b in &values_non_nan {
-                        let res = a.get().partial_cmp(&b.get());
-                        let reversed = b.get().partial_cmp(&a.get());
-
-                        assert_eq!(res, a.partial_cmp(b));
-                        assert_eq!(reversed, b.partial_cmp(a));
-                    }
-
-                    for b in &values_non_zero_non_nan {
-                        let res = a.get().partial_cmp(&b.get());
-                        let reversed = b.get().partial_cmp(&a.get());
-
-                        assert_eq!(res, a.partial_cmp(b));
-                        assert_eq!(reversed, b.partial_cmp(a));
-                    }
-
-                    for b in &tf64::get_test_values() {
-                        let res = a.get().partial_cmp(b);
-                        let reversed = b.partial_cmp(&a.get());
-
-                        assert_eq!(res, a.partial_cmp(b));
-                        assert_eq!(reversed, b.partial_cmp(a));
-                    }
-                }
-            }
-        }
     };
 }
 
-impl_ord!(non_nan, NonNaN);
-impl_ord!(non_zero_non_nan, NonZeroNonNaN);
-impl_ord!(non_nan_finite, NonNaNFinite);
-impl_ord!(non_zero_non_nan_finite, NonZeroNonNaNFinite);
-impl_ord!(positive, Positive);
-impl_ord!(negative, Negative);
-impl_ord!(positive_finite, PositiveFinite);
-impl_ord!(negative_finite, NegativeFinite);
-impl_ord!(strictly_positive, StrictlyPositive);
-impl_ord!(strictly_negative, StrictlyNegative);
-impl_ord!(strictly_positive_finite, StrictlyPositiveFinite);
-impl_ord!(strictly_negative_finite, StrictlyNegativeFinite);
+impl_ord!(NonNaN);
+impl_ord!(NonZeroNonNaN);
+impl_ord!(NonNaNFinite);
+impl_ord!(NonZeroNonNaNFinite);
+impl_ord!(Positive);
+impl_ord!(Negative);
+impl_ord!(PositiveFinite);
+impl_ord!(NegativeFinite);
+impl_ord!(StrictlyPositive);
+impl_ord!(StrictlyNegative);
+impl_ord!(StrictlyPositiveFinite);
+impl_ord!(StrictlyNegativeFinite);
+
+impl_partial_ord!(NonNaN);
+impl_partial_ord!(NonZeroNonNaN);
+impl_partial_ord!(NonNaNFinite);
+impl_partial_ord!(NonZeroNonNaNFinite);
+impl_partial_ord!(Positive);
+impl_partial_ord!(Negative);
+impl_partial_ord!(PositiveFinite);
+impl_partial_ord!(NegativeFinite);
+impl_partial_ord!(StrictlyPositive);
+impl_partial_ord!(StrictlyNegative);
+impl_partial_ord!(StrictlyPositiveFinite);
+impl_partial_ord!(StrictlyNegativeFinite);
---
+++ typed_floats/tests/ord.rs
@@ -0,0 +1,141 @@
+macro_rules! impl_ord_test {
+    ($test:ident, $type:ident) => {
+        mod $test {
+            extern crate std;
+            use typed_floats::*;
+            use std::vec::Vec; // Required for the tests to compile in no_std mode
+
+            fn is_sorted<T: PartialOrd>(slice: &[T]) -> bool {
+                slice.windows(2).all(|w| w[0] <= w[1])
+            }
+
+            #[test]
+            fn f32() {
+                let min_positive = tf32::MIN_POSITIVE;
+                let max = tf32::MAX;
+                let infinity = tf32::INFINITY;
+
+                assert!(min_positive < max);
+                assert!(min_positive < infinity);
+                assert!(max < infinity);
+
+                let values = tf32::get_test_values()
+                    .iter()
+                    .map(|&x| tf32::$type::new(x))
+                    .filter_map(|x| x.ok())
+                    .collect::<Vec<_>>();
+
+                assert!(is_sorted(&values));
+                let floats = values.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(is_sorted(&floats));
+
+                let reversed = values.iter().rev().collect::<Vec<_>>();
+
+                assert!(!is_sorted(&reversed));
+                let floats = reversed.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(!is_sorted(&floats));
+
+                let mut sorted = reversed.clone();
+                sorted.sort();
+
+                assert!(is_sorted(&sorted));
+                let floats = sorted.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(is_sorted(&floats));
+            }
+
+            #[test]
+            fn f64() {
+                let min_positive = tf64::MIN_POSITIVE;
+                let max = tf64::MAX;
+                let infinity = tf64::INFINITY;
+
+                assert!(min_positive < max);
+                assert!(min_positive < infinity);
+                assert!(max < infinity);
+
+                let values = tf64::get_test_values()
+                    .iter()
+                    .map(|&x| tf64::$type::new(x))
+                    .filter_map(|x| x.ok())
+                    .collect::<Vec<_>>();
+
+                assert!(is_sorted(&values));
+                let floats = values.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(is_sorted(&floats));
+
+                let reversed = values.iter().rev().collect::<Vec<_>>();
+
+                assert!(!is_sorted(&reversed));
+                let floats = reversed.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(!is_sorted(&floats));
+
+                let mut sorted = reversed.clone();
+                sorted.sort();
+
+                assert!(is_sorted(&sorted));
+                let floats = sorted.iter().map(|x| x.get()).collect::<Vec<_>>();
+                assert!(is_sorted(&floats));
+            }
+
+            #[test]
+            fn cmp_with_others() {
+                let values_non_nan = tf64::get_test_values()
+                    .iter()
+                    .map(|&x| tf64::NonNaN::new(x))
+                    .filter_map(|x| x.ok())
+                    .collect::<Vec<_>>();
+
+                let values_non_zero_non_nan = tf64::get_test_values()
+                    .iter()
+                    .map(|&x| tf64::NonZeroNonNaN::new(x))
+                    .filter_map(|x| x.ok())
+                    .collect::<Vec<_>>();
+
+                let values = tf64::get_test_values()
+                    .iter()
+                    .map(|&x| tf64::$type::new(x))
+                    .filter_map(|x| x.ok())
+                    .collect::<Vec<_>>();
+
+                for a in &values {
+                    for b in &values_non_nan {
+                        let res = a.get().partial_cmp(&b.get());
+                        let reversed = b.get().partial_cmp(&a.get());
+
+                        assert_eq!(res, a.partial_cmp(b));
+                        assert_eq!(reversed, b.partial_cmp(a));
+                    }
+
+                    for b in &values_non_zero_non_nan {
+                        let res = a.get().partial_cmp(&b.get());
+                        let reversed = b.get().partial_cmp(&a.get());
+
+                        assert_eq!(res, a.partial_cmp(b));
+                        assert_eq!(reversed, b.partial_cmp(a));
+                    }
+
+                    for b in &tf64::get_test_values() {
+                        let res = a.get().partial_cmp(b);
+                        let reversed = b.partial_cmp(&a.get());
+
+                        assert_eq!(res, a.partial_cmp(b));
+                        assert_eq!(reversed, b.partial_cmp(a));
+                    }
+                }
+            }
+        }
+    };
+}
+
+impl_ord_test!(ord_non_nan, NonNaN);
+impl_ord_test!(ord_non_zero_non_nan, NonZeroNonNaN);
+impl_ord_test!(ord_non_nan_finite, NonNaNFinite);
+impl_ord_test!(ord_non_zero_non_nan_finite, NonZeroNonNaNFinite);
+impl_ord_test!(ord_positive, Positive);
+impl_ord_test!(ord_negative, Negative);
+impl_ord_test!(ord_positive_finite, PositiveFinite);
+impl_ord_test!(ord_negative_finite, NegativeFinite);
+impl_ord_test!(ord_strictly_positive, StrictlyPositive);
+impl_ord_test!(ord_strictly_negative, StrictlyNegative);
+impl_ord_test!(ord_strictly_positive_finite, StrictlyPositiveFinite);
+impl_ord_test!(ord_strictly_negative_finite, StrictlyNegativeFinite);
