0adb1ff0a96216b2ed1d84e8c6550b83fdd78167
Tom <1955774+tdelmas@users.noreply.github.com>
2026-02-14 22:07:23 +0000

    Skip doctest
    Signed-off-by: GitHub <noreply@github.com>

--- typed_floats/tests.sh
+++ typed_floats/tests.sh
@@ -18,20 +18,22 @@ cargo build --no-default-features --features serde,libm
 cargo build --no-default-features --features std,libm
 cargo build --no-default-features --features serde,std,libm
 
-cargo test --no-default-features
-cargo test --no-default-features --features std
-cargo test --no-default-features --features serde
-cargo test --no-default-features --features libm
-cargo test --no-default-features --features serde,std
-cargo test --no-default-features --features serde,libm
-cargo test --no-default-features --features std,libm
-cargo test --no-default-features --features serde,std,libm
+cargo test --lib --bins --tests --no-default-features --features f32
+cargo test --lib --bins --tests --no-default-features --features f64
+cargo test --lib --bins --tests --no-default-features --features f32,std
+cargo test --lib --bins --tests --no-default-features --features f32,serde
+cargo test --lib --bins --tests --no-default-features --features f32,libm
+cargo test --lib --bins --tests --no-default-features --features f32,serde,std
+cargo test --lib --bins --tests --no-default-features --features f32,serde,libm
+cargo test --lib --bins --tests --no-default-features --features f32,std,libm
+cargo test --lib --bins --tests --no-default-features --features f32,serde,std,libm
 
-cargo test --release --no-default-features
-cargo test --release --no-default-features --features std
-cargo test --release --no-default-features --features serde
-cargo test --release --no-default-features --features libm
-cargo test --release --no-default-features --features serde,std
-cargo test --release --no-default-features --features serde,libm
-cargo test --release --no-default-features --features std,libm
-cargo test --release --no-default-features --features serde,std,libm
+cargo test --lib --bins --tests --release --no-default-features --features f32
+cargo test --lib --bins --tests --release --no-default-features --features f64
+cargo test --lib --bins --tests --release --no-default-features --features f32,std
+cargo test --lib --bins --tests --release --no-default-features --features f32,serde
+cargo test --lib --bins --tests --release --no-default-features --features f32,libm
+cargo test --lib --bins --tests --release --no-default-features --features f32,serde,std
+cargo test --lib --bins --tests --release --no-default-features --features f32,serde,libm
+cargo test --lib --bins --tests --release --no-default-features --features f32,std,libm
+cargo test --lib --bins --tests --release --no-default-features --features f32,serde,std,libm
