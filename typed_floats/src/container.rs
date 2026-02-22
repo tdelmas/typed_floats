72e252870a7d99d38240bb02b1e39249b3d37c3b
Tom Delmas <1955774+tdelmas@users.noreply.github.com>
2026-02-22 15:48:30 +0100

    Serde
    Signed-off-by: Tom Delmas <1955774+tdelmas@users.noreply.github.com>

--- typed_floats/src/container.rs
+++ typed_floats/src/container.rs
@@ -1,10 +1,22 @@
 // mod layout_optimisation;
 
+#[cfg(feature = "serde")]
+use serde::Serialize;
+
+#[cfg(feature = "serde")]
+pub trait Contained: Serialize {}
+#[cfg(not(feature = "serde"))]
+pub trait Contained {}
+
+impl Contained for f32 {}
+impl Contained for f64 {}
+
+#[cfg_attr(feature = "serde", derive(Serialize))]
 #[derive(Debug, Copy, Clone)]
 #[repr(transparent)]
-pub(crate) struct Container<T> (T);
+pub(crate) struct Container<T: Contained> (T);
 
-impl<T> Container<T> {
+impl<T: Contained> Container<T> {
     pub const fn new(value: T) -> Container<T> {
         Self(value)
     }
